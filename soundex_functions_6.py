5db293770fe1cceca1f97f0deee490583894a744
mitra999 <57156351+mitra999@users.noreply.github.com>
2021-11-03 10:16:20 -0400

    Update soundex_functions_6.py

--- soundex_functions_6.py
+++ soundex_functions_6.py
@@ -1,4 +1,29 @@
-import encode as en
+# =============================================================================
+
+"""Module encode.py - Various phonetic name encoding methods.
+Encoding methods provided:
+  soundex         Soundex
+  mod_soundex     Modified Soundex
+  phonex          Phonex
+  nysiis          NYSIIS
+  dmetaphone      Double-Metaphone
+  phonix          Phonix
+  fuzzy_soundex   Fuzzy Soundex based on q-gram substitutions and letter
+                  encodings
+  get_substring   Simple function which extracts and returns a sub-string
+  freq_vector     Count characters and put into a vector
+There is also a routine called 'phonix_transform' which only performs the
+Phonix string transformation without the final numerical encoding. This can
+be useful for approximate string comparison functions.
+Note that all encoding routines assume the input string only contains letters
+and whitespaces, but not digits or other ASCII characters.
+If called from the command line, a test routine is run which prints example
+encodings for various strings.
+"""
+
+# =============================================================================
+
+import encode as en # Module encode.py repository for more detailed documentation
 import csv
 import pandas as pd
 def Soundex (df): 
