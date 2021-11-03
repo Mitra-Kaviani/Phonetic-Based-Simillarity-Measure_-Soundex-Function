fb80c53fc2d548b20b60a89036edefb2ccd64b5d
mitra999 <57156351+mitra999@users.noreply.github.com>
2021-11-03 01:13:39 -0400

    Add files via upload

---
+++ soundex_functions_6.py
@@ -0,0 +1,15 @@
+import encode as en
+import csv
+import pandas as pd
+def Soundex (df): 
+    n1 = list(df)[0] #first string label
+    n2 = list(df)[1] #second string label
+    df.loc[0:,'Soundex1']=0
+    df.loc[0:,'Soundex2']=0
+    for i in range(0,len(df)):
+        df.loc[i,'Soundex1']=en.soundex(str(df.loc[i,n1]),maxlen=4)
+        df.loc[i,'Soundex2']=en.soundex(str(df.loc[i,n2]),maxlen=4) 
+    return df
+df1 = pd.read_csv('C:/Users/Mitra/Desktop/dataset3_FNS_MITRA - Copy.csv', sep=',', skipinitialspace=True)
+df1= Soundex (df1)
+print (df1)
